b7aa13a30fdaefd11bd4c8ae551ef6b124e31990
d0190535 <d0190535@gmail.com>
2015-10-12 19:56:27 -0500

    adding new components.

--- ReactJsPlayground/Scripts/Tutorial.jsx
+++ ReactJsPlayground/Scripts/Tutorial.jsx
@@ -3,11 +3,38 @@
         return (
             <div className="commentBox">
                 Hello World!  I am a comment box yay
+
+                Adding new react components
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
             </div>
             );
     }
 });
 
+var CommentList = React.createClass({
+    render: function () {
+        return (
+            <div className="commentList">
+                I am a comment list woot
+            </div>
+            );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function () {
+        return (
+            <div className="commentForm">
+                I am a comment form awesome
+            </div>
+            );
+    }
+});
+
+
+// seems like render has to be placed at the end
 React.render(
     <CommentBox />,
     document.getElementById('content')
